5cb4532bf392e10376c73ee4d3f79ae5e4372657
doodle26 <56640630+doodle26@users.noreply.github.com>
2019-10-17 23:11:19 +0530

    Add files via upload

---
+++ trig_d.sql
@@ -0,0 +1,12 @@
+drop trigger if exists Make_audit1;
+delimiter //
+CREATE TRIGGER Make_audit1
+  AFTER DELETE ON lib_book
+  FOR EACH ROW
+BEGIN
+  INSERT INTO lib_Audit (bid, bname, qty)
+  VALUES (OLD.bid, OLD.bname,OLD.qty);
+END;
+//
+delimiter ;
+
